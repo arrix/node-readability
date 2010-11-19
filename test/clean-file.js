25a031147576865e8a22885cbbda03c2ef955910
Arrix <arrixzhou@gmail.com>
2010-11-19 19:27:47 +0800

    grab pages from rss sources for testing

--- test/clean-file.js
+++ test/clean-file.js
@@ -1,16 +1,41 @@
 var http = require('http'),
     url_mod = require('url'),
     fs = require('fs');
-    
-var readability = require('../lib/readability-module.js');
+        
+var readability = require('../lib/readability.js'),
+    sprintf = readability.sprintf;
 
-function cleanFile(path, cb) {
+function cleanFile(path, url, cb) {
     var content = fs.readFileSync(path, 'utf-8');
-    readability.parse(content, cb);
+    readability.parse(content, url, cb);
 }
 
-cleanFile('test/nytime.html', function(info) {
-    //console.log(info.content);
-});
+// cleanFile(__dirname + '/pages/http___www_youtube_com_watch_v_HBnNoF71W2Q.html', '', function(info) {
+//     //console.log(info.content);
+// });
+// 
+// process.exit();
 
+function batch_run() {
+    var dir = __dirname + '/pages/';
+    var files = fs.readdirSync(dir);
+    var results = [];
+    //files.length = 10;
+    //files[0] = files.pop(); files.length = 1;
+    files.forEach(function(f) {
+        console.log('######## Processing file...', f);
+        cleanFile(dir + f, '', function(result) {
+            results.push({time: result.time, file: f, inputLength: result.inputLength, error: result.error});
+        });
+    });
+    
+    var total = 0, totalTime = 0;
+    results.filter(function(v) {return !v.error}).sort(function(a, b) {return a - b;}).forEach(function(r) {
+        total++;
+        totalTime += r.time;
+        console.log(sprintf('%5.2f\t%8d\t%10s', r.time, r.inputLength, r.file));
+    });
+    console.log('total:', total, "avg time:", totalTime/total);
+}
 
+batch_run();
---
+++ test/grab-pages.rb
@@ -0,0 +1,58 @@
+require 'open-uri'
+require 'rexml/document'
+require 'fileutils'
+
+module Program
+  class << self
+    def fetch_feed(url)
+      content = nil
+      open(url) do |f|
+        content = f.read
+      end
+      content
+    end
+    
+    def fetch_digg_feed
+      url = 'http://services.digg.com/2.0/story.getTopNews?type=rss'
+      content = fetch_feed(url)
+      content.force_encoding('iso-8859-1');
+      content.encode('utf-8')
+    end
+    
+    def fetch_hackernews_feed
+      url = 'http://news.ycombinator.com/rss'
+      fetch_feed(url)
+    end
+    
+    def fetch_delicious_feed
+      url = 'http://feeds.delicious.com/v2/rss/?count=30'
+      fetch_feed(url)
+    end
+    
+    def parse_rss(feed)
+      xml = REXML::Document.new(feed)
+      xml.elements.each("//item") do |item|
+        link = item.get_elements('link')[0].text.strip
+        title = item.get_elements('title')[0].text.strip
+        yield link, title
+      end
+    end
+    
+    def run
+      dir = File.expand_path('../pages', __FILE__)
+      FileUtils.mkdir(dir) unless File.exists? dir
+
+      parse_rss(fetch_digg_feed) do |url, title|
+        filename = title.gsub(/\W/, '_') + '.html'
+        filepath = File.join(dir, filename)
+        puts "fetching #{url} as #{filepath}"
+        puts `curl #{url} > #{filepath} &`
+      end
+    end
+    
+  end
+end
+
+if __FILE__ == $0
+  Program.run
+end
